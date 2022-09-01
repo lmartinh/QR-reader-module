667e2a0c7f79f5a7e36a28a9c9ba35091e2b58c8
Leticia M <lmartin@facephi.com>
2022-09-01 19:04:53 +0200

    App main activity

--- qrreader/src/main/java/com/lmartinh/qrreader/internal/Extensions.kt
+++ qrreader/src/main/java/com/lmartinh/qrreader/internal/Extensions.kt
@@ -2,7 +2,7 @@ package com.lmartinh.qrreader.internal
 
 import java.nio.ByteBuffer
 
-fun ByteBuffer.toByteArray(): ByteArray {
+internal fun ByteBuffer.toByteArray(): ByteArray {
     rewind()
     val data = ByteArray(remaining())
     get(data)
